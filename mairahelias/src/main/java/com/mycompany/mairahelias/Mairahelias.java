c8ea1205a92e10364c8efa40a88cad13f701c6bc
aluno <aluno@lab02-05>
2026-03-02 14:08:52 -0300

    minha primeira atualização

---
+++ mairahelias/src/main/java/com/mycompany/mairahelias/Mairahelias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mairahelias;
+
+/**
+ *
+ * @author aluno
+ */
+public class Mairahelias {
+
+    public static void main(String[] args) {
+        System.out.println("ola mundo!");
+    }
+}
