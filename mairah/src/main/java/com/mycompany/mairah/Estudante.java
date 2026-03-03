36551c1540a469695960893664bc6e4b9b63d002
aluno <aluno@lab01-10>
2026-03-03 11:17:10 -0300

    segunda atualizacao

---
+++ mairah/src/main/java/com/mycompany/mairah/Estudante.java
@@ -0,0 +1,30 @@
+package com.mycompany.mairah;
+
+import java.util.Date;
+
+public class Estudante {
+
+    private String nome;
+    private Date dataNascimento;
+    private String cpf;
+    private char genero;
+    private String matricula;
+    private String email;
+    
+    public Estudante () {
+        this.nome = "Nodody";
+    }
+
+    public int obterIdade(Date hoje) {
+        int idade = 0;
+        // Lógica para calcular idade
+        return idade;
+    }
+    public String getNome (){
+    return this.nome;
+    }
+    public void setNome(String set_nome){
+    this.nome = set_nome;
+    }
+ 
+}
